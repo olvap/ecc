7289ac82e26e8a44131e9f2fe714983a674c5c21
Mauricio Voto <mauriciovoto@gmail.com>
2013-07-27 11:12:52 -0300

    annotate + tags style with heart image

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :integer          not null, primary key
+#  address    :string(255)
+#  phone      :string(255)
+#  cel_phone  :string(255)
+#  email      :string(255)
+#  church     :string(255)
+#  health_obs :text
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 # -*- encoding : utf-8 -*-
 class Team < ActiveRecord::Base
   include Person
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: participants
+#
+#  id         :integer          not null, primary key
+#  address    :string(255)
+#  phone      :string(255)
+#  cel_phone  :string(255)
+#  email      :string(255)
+#  church     :string(255)
+#  couple     :string(255)
+#  health_obs :text
+#  children   :integer
+#  name       :string(255)
+#  full_name  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Participant do
