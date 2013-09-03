5aac52e8f7af663724286110f5aa0bce7c60ee1b
Mauricio Voto <mauriciovoto@gmail.com>
2013-09-03 13:27:48 -0300

    change user auth to get local env variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,9 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate
   protect_from_forgery
 
-  SETTINGS = HashWithIndifferentAccess.new( YAML::load(File.open("#{::Rails.root}/config/settings.yml")) )
-
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == SETTINGS[:user] && password == SETTINGS[:password]
+      username == ENV['ECCUSER'] && password == ENV['ECCPASSWORD']
     end
   end
 end
