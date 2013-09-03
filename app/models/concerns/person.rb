70be118d3457647bc146d3c978318c160bbed6a0
Mauricio Voto <mauriciovoto@gmail.com>
2013-09-03 13:40:49 -0300

    add default scope ordering by name

--- app/models/concerns/person.rb
+++ app/models/concerns/person.rb
@@ -5,10 +5,11 @@ module Person
   included do
     attr_accessible :address, :phone, :cel_phone, :church, :email, :health_obs, :name
     validates_presence_of :name
+    default_scope order(:name)
   end
 
   def wife_name
     arr = name.split(" da ")
     arr[1] + " do " + arr[0]
-  end  
+  end
 end
