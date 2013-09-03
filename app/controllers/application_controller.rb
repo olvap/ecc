b6f247160dd819f494522a09e63f918ce37921e9
Mauricio Voto <mauriciovoto@gmail.com>
2013-09-03 12:43:43 -0300

    add http basic and very simple authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+require 'yaml'
+
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   protect_from_forgery
+
+  SETTINGS = HashWithIndifferentAccess.new( YAML::load(File.open("#{::Rails.root}/config/settings.yml")) )
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == SETTINGS[:user] && password == SETTINGS[:password]
+    end
+  end
 end
