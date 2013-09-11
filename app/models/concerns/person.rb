563075d4a794b260838396092cac559d479a5f22
Mauricio Voto <mauriciovoto@gmail.com>
2013-09-11 12:49:16 -0300

    add validation of name format

--- app/models/concerns/person.rb
+++ app/models/concerns/person.rb
@@ -5,6 +5,7 @@ module Person
   included do
     attr_accessible :address, :phone, :cel_phone, :church, :email, :health_obs, :name
     validates_presence_of :name
+    validates_format_of :name, with: /[a-zA-Z]+\s{1}da{1}\s{1}[a-zA-Z]+/, message: "verifique o formato, exemplo: Carlos da Clotilde"
     default_scope order(:name)
   end
 
